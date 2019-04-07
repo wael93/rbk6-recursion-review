5bb2e5d7f3c43339c41bff6fe4719e28bcc771e7
RBK-5 <rbk-5@RBK-5s-Mac-mini.Dlink>
2019-04-07 14:06:27 +0300

    sprint1Recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,66 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+test = {eat:"apple", numb: 2};
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+ if(typeof obj === 'number'){
+ 	obj =  obj + "";
+ }
+ else if (obj === null){
+ 	obj = "null";
+ }
+
+ else if (typeof obj === 'boolean'){
+ 	if (obj === true){
+ 		obj = 'true';
+ 	}else{
+ 		obj = 'false';
+ 	}
+ }
+ else if (typeof obj === 'string'){
+ 	obj = '"' + obj + '"';
+
+}
+	else if (Array.isArray(obj)){
+		if(obj.length === 0){
+		obj = '[]';
+		}
+		else if (obj.length === 1 && typeof obj[0] === 'number'){
+		var str = '';
+		str += '[' +  obj.toString() + ']' ;
+		return str;
+
+		}
+		else if (obj.length === 1 && typeof obj[0] === 'string'){
+		var str = '';
+		str += '[' + '"' + obj.toString() + '"' + ']' ;
+		return str;
+		}
+		else if (obj.length === 2 && typeof obj[0] === 'number'){
+		var str = '';
+		str += '[' + obj[0].toString() + ',' + '"' + obj[1].toString() + '"' + ']' ;
+		return str;
+		}
+		else if (obj.length > 1 && typeof obj[0] === 'number'){
+		var str = '';
+		str += '[' +  obj.toString() + ']' ;
+		return str;
+		}
+	}
+
+return obj; 
 };
+
+
+
+
+
+
+   
+
+
+ 
+
+
